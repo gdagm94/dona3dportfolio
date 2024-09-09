9f9b9e074f1fd50fad738197b3bfb97a51e8b98d
gdagm94 <65061362+gdagm94@users.noreply.github.com>
2024-09-09 14:22:08 -0400

    C

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: 'gdagm94/dona3dportfolio/', // This should match your GitHub repository name
+  base: '/dona3dportfolio/', // This should match your GitHub repository name
 });
